a284e9537a936e132000330e93faa5a88204a78f
Keijiro Takahashi <keijiro@gmail.com>
2016-08-13 14:09:08 +0900

    Total redesign

---
+++ Assets/CursorController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    [SerializeField] RectTransform _mouseCursor;
+    [SerializeField] RectTransform[] _touchCursors;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _mouseCursor.anchoredPosition = Input.mousePosition;
+            _mouseCursor.gameObject.SetActive(true);
+        }
+        else
+        {
+            _mouseCursor.gameObject.SetActive(false);
+        }
+
+        var touches = Input.touches;
+        var i = 0;
+
+        for (; i < touches.Length; i++)
+        {
+            _touchCursors[i].anchoredPosition = touches[i].position;
+            _touchCursors[i].gameObject.SetActive(true);
+        }
+
+        for (; i < _touchCursors.Length; i++)
+            _touchCursors[i].gameObject.SetActive(false);
+    }
+}
---
+++ Assets/DisplayActivator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class DisplayActivator : MonoBehaviour
+{
+    [SerializeField] Camera[] _cameras;
+
+    void Start()
+    {
+        var displays = Display.displays;
+        for (var i = 1; i < displays.Length; i++)
+        {
+            displays[i].Activate();
+            _cameras[i].targetDisplay = i;
+            _cameras[i].enabled = true;
+        }
+    }
+}
